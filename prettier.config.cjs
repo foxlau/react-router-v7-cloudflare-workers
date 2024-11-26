03897a88d4021f5ecd94231853e70d67241b9289
foxlau <3970099+foxlau@users.noreply.github.com>
2024-11-27 07:26:51 +0800

    Replace Prettier import sorting plugin

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,21 +1,7 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  printWidth: 80,
   tabWidth: 2,
-  endOfLine: "lf",
-  importOrder: [
-    "^(react/(.*)$)|^(react$)",
-    "^react-router",
-    "^@react-router/(.*)$",
-    "<THIRD_PARTY_MODULES>",
-    "",
-    "^~/(.*)$",
-    "^[./]",
-  ],
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderTypeScriptVersion: "5.0.0",
-  plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
-  ],
+  printWidth: 80,
+  trailingComma: "all",
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
 };
